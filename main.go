80dc62f84b1e9af35084d70a4a3c785ce2518229
livia <livia.gu@notitia.consulting>
2023-11-04 20:09:47 +1030

    update cors policy

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Expose-Headers", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Type, Authorization, Accept, Accept-Language, X-Requested-With, XMLHttpRequest")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, Accept-Language, X-Requested-With, XMLHttpRequest")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "false")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")	
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
